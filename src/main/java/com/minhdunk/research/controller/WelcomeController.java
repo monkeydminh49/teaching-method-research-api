e8daf7b755564bdc36d67cfa3ef093c55df4e5a2
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2024-01-14 22:28:50 +0700

    Disable cors

--- src/main/java/com/minhdunk/research/controller/WelcomeController.java
+++ src/main/java/com/minhdunk/research/controller/WelcomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class WelcomeController {
 
     @GetMapping("/hello")
