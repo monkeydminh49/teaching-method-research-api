954f9d13a97662a0e2dd34e72582dd1e780b1867
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2024-01-17 17:41:24 +0700

    Update jwt swagger

--- src/main/java/com/minhdunk/research/config/OpenApiConfig.java
+++ src/main/java/com/minhdunk/research/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.minhdunk.research.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,12 @@ public class OpenApiConfig {
 
     @Value("${bezkoder.openapi.prod-url}")
     private String prodUrl;
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 
     @Bean
     public OpenAPI myOpenAPI() {
@@ -46,6 +55,9 @@ public class OpenApiConfig {
                 .description("This API exposes endpoints to manage demo.")
                 .license(mitLicense);
 
-        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+        return new OpenAPI().info(info).servers(List.of(devServer, prodServer))
+                .addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
     }
 }
