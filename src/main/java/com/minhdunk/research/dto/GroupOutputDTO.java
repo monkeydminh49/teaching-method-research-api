5f7a3ac925ae7fec6e03e21fbd81a483d806c945
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2024-01-26 00:48:00 +0700

    update server

---
+++ src/main/java/com/minhdunk/research/dto/GroupOutputDTO.java
@@ -0,0 +1,10 @@
+package com.minhdunk.research.dto;
+
+
+import java.util.List;
+
+public class GroupOutputDTO {
+    public Long id;
+    public AssignmentOutputDTO assignment;
+    public List<UserOutputDTO> students;
+}
