661ed76ba1f0940ccff7829a10bf5d72beb28059
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2024-04-22 00:33:25 +0700

    Update CounsellingController and test cd

--- src/main/java/com/minhdunk/research/controller/CounsellingController.java
+++ src/main/java/com/minhdunk/research/controller/CounsellingController.java
@@ -5,11 +5,10 @@ import com.minhdunk.research.mapper.CounsellingMapper;
 import com.minhdunk.research.service.CounsellingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@CrossOrigin
+@RestController
 @RequestMapping("/api/v1/counsellings")
 public class CounsellingController {
     @Autowired
